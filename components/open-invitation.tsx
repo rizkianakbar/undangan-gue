56f57e5fec83b87e29ba527a8895590f95614271
rizkianakbar <rizkianakbar4@gmail.com>
2022-05-20 13:45:47 +0700

    fix(ui): modify position

--- components/open-invitation.tsx
+++ components/open-invitation.tsx
@@ -14,17 +14,19 @@ const OpenInvitation = () => {
   const { setOpen } = React.useContext(AppContext);
 
   return (
-    <div className="z-50 md:h-[100vh] bg-[#E2C6C6] mx-auto flex flex-col items-center justify-center pt-10">
-      <Image src={cover} alt="cover-image" width="280px" height="320px" />
+    <div className="z-50 h-[100vh] bg-[#E2C6C6] mx-auto flex flex-col items-center justify-center pt-1">
+      <Image src={cover} alt="cover-image" width="243px" height="280px" />
       <div className="text-center text-gray-500 text-xl w-[300px] font-formal">
-        <p className="mb-4">The Wedding Of</p>
-        <p className="font-gaya text-5xl mb-6">Vinni & Rifqi</p>
-        <p className="myt-4">Kepada Yth</p>
-        <p className="mb-4">Bapak/Ibu/Saudara/i</p>
+        <p className="mb-4 text-lg">The Wedding Of</p>
+        <p className="font-gaya text-5xl mb-4">
+          Vinni <span className="text-white">&</span> Rifqi
+        </p>
+        <p className="text-lg">Kepada Yth</p>
+        <p className="mb-1 text-lg">Bapak/Ibu/Saudara/i</p>
         <div className="w-full h-20 p-4 bg-white rounded-lg">
           <p className="mt-3">{name}</p>
         </div>
-        <p className="text-sm mt-4">
+        <p className="text-sm mt-2">
           Mohon maaf apabila ada kesalahan penulisan nama/gelar
         </p>
         <button
