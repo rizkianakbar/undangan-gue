7f98d1c2390a7f6c00c9d48fc21f565e3503957b
rizkianakbar <rizkianakbar4@gmail.com>
2022-05-20 18:01:34 +0700

    fix: something

--- components/layout/pages/page-section.tsx
+++ components/layout/pages/page-section.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 
 export const PageSection: React.FC = ({ children }) => {
-  return <section className="flex flex-col flex-1 ">{children}</section>;
+  return (
+    <section className="flex flex-col flex-1 scroll-smooth">{children}</section>
+  );
 };
