c51fd0cd9b5338b5ad6eff72fccd9239cf6c8755
rizkianakbar <rizkianakbar4@gmail.com>
2022-05-21 17:55:24 +0700

    feat: add animation

--- components/open-invitation.tsx
+++ components/open-invitation.tsx
@@ -2,21 +2,41 @@ import { AppContext } from '@/pages/_app';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 import React from 'react';
+import { motion } from 'framer-motion';
 
 import { cover } from '../assets/img';
 
 const OpenInvitation = () => {
   const router = useRouter();
   const { to } = router.query;
-
   const name = to ? (to as string).split('-').join(' ') : '';
-
   const { setOpen } = React.useContext(AppContext);
+
+  const containerVariants = {
+    hidden: {
+      opacity: 0,
+      y: -100,
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: 0.5,
+        duration: 0.8,
+        type: 'spring',
+      },
+    },
+  };
 
   return (
     <div className="z-50 section2 mx-auto flex flex-col items-center justify-center">
       <Image src={cover} alt="cover-image" width="243px" height="280px" />
-      <div className="text-center text-gray-500 text-xl w-[300px] font-formal">
+      <motion.div
+        variants={containerVariants}
+        initial="hidden"
+        animate="visible"
+        className="text-center text-gray-500 text-xl w-[300px] font-formal"
+      >
         <p className="mb-4 text-lg">The Wedding Of</p>
         <p className="font-gaya text-5xl mb-6">
           Vinni <span className="text-pink-100">&</span> Rifqi
@@ -35,7 +55,7 @@ const OpenInvitation = () => {
         >
           buka undangan
         </button>
-      </div>
+      </motion.div>
     </div>
   );
 };
