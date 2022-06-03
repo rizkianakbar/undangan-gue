97b1687b20e1f8af90f38cebb55bc3e6a991173e
rizkianakbar <rizkianakbar4@gmail.com>
2022-06-04 00:45:07 +0700

    refactor: add type definitions and remove unused codes

--- components/pagination.tsx
+++ components/pagination.tsx
@@ -1,59 +1,73 @@
 import { AppContext } from '@/pages/_app';
 import { fetcher } from '@/utils/fetcher';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import ReactPaginate from 'react-paginate';
 import ReactTimeAgo from 'react-time-ago';
 
-function Items({ currentItems }: any) {
+interface Props {
+  itemsPerPage: number;
+}
+
+interface ICurrentItems {
+  id: number;
+  name: string;
+  createdAt: number | Date;
+  comment: string;
+}
+
+interface IPage {
+  selected: number;
+}
+
+function Items({ currentItems }: { currentItems: ICurrentItems[] }) {
   return (
     <>
-      {currentItems &&
-        currentItems.map((item: any) => (
-          <div className="flex text-left my-2" key={item.id}>
-            <div className="flex-1 border rounded-lg px-4 py-2 sm:px-6 sm:py-4 leading-relaxed">
-              <strong>{item.name}</strong>{' '}
-              <span className="text-xs text-gray-400">
-                <ReactTimeAgo date={item.createdAt} locale="id" />
-              </span>
-              <p className="text-sm">{item.comment}</p>
-            </div>
+      {currentItems?.map((item: ICurrentItems) => (
+        <div className="flex text-left my-2" key={item.id}>
+          <div className="flex-1 border rounded-lg px-4 py-2 sm:px-6 sm:py-4 leading-relaxed">
+            <strong>{item.name}</strong>{' '}
+            <span className="text-xs text-gray-400">
+              <ReactTimeAgo date={item.createdAt} locale="id" />
+            </span>
+            <p className="text-sm">{item.comment}</p>
           </div>
-        ))}
+        </div>
+      ))}
     </>
   );
 }
 
-const PaginatedItems = ({ itemsPerPage }: any) => {
+const PaginatedItems = (props: Props) => {
+  const { itemsPerPage } = props;
   // We start with an empty list of items.
-  const [currentItems, setCurrentItems] = useState(null);
+  const [currentItems, setCurrentItems] = useState<ICurrentItems[]>([]);
   const [pageCount, setPageCount] = useState(0);
   // Here we use item offsets; we could also use page offsets
   // following the API or data you're working with.
   const [itemOffset, setItemOffset] = useState(0);
-  const { setLoading, reload, data, setData } = React.useContext(AppContext);
+  const { setLoading, reload, data, setData } = useContext(AppContext);
 
-  React.useEffect(() => {
+  useEffect(() => {
     const getComments = async () => {
-      await fetcher('/api/get-comment').then((data: any) => {
+      await fetcher('/api/get-comment').then((data: string[]) => {
         setData(data);
         setLoading(false);
       });
     };
+
     getComments();
   }, [reload, setData, setLoading]);
 
   useEffect(() => {
     const endOffset = itemOffset + itemsPerPage;
-    console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+
     setCurrentItems(data.slice(itemOffset, endOffset));
     setPageCount(Math.ceil(data.length / itemsPerPage));
   }, [data, itemOffset, itemsPerPage]);
 
-  const handlePageClick = (event: any) => {
+  const handlePageClick = (event: IPage) => {
     const newOffset = (event.selected * itemsPerPage) % data.length;
-    console.log(
-      `User requested page number ${event.selected}, which is offset ${newOffset}`
-    );
+
     setItemOffset(newOffset);
   };
 
